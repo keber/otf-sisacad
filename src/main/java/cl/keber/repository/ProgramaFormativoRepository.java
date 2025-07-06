6b90808cd0be537f4b04a54ae34a58c8ec1bb2dc
Keber Flores <keberflores@gmail.com>
2025-07-06 17:20:18 -0400

    fix: add ProgramaFormativoRepository [GREEN]

---
+++ src/main/java/cl/keber/repository/ProgramaFormativoRepository.java
@@ -0,0 +1,7 @@
+package cl.keber.repository;
+
+import cl.keber.model.ProgramaFormativo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProgramaFormativoRepository extends JpaRepository<ProgramaFormativo, Long> {
+}
